f7afbf605e779026303e9ff8c1ee1c9756ee47fb
Lanrutcon <Lanrutcon@users.noreply.github.com>
2024-09-03 22:36:03 +0100

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='DegiroAPI',
-    version='1.0.4',
+    version='1.0.5',
     description='Unofficial DEGIRO API for Python',
     url='https://github.com/lanrutcon/DegiroAPI-Python',
     author='lanrutcon',
